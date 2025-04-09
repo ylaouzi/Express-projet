bcf32840b6c89c99e911d6e912c60cbabe3d8c39
ylaouzi <laouziyassine@gmail.com>
2025-04-09 21:33:22 +0100

    front commit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
-import express from 'express';
-import userRoutes from './routes/user-routes';
+import express, { Request, Response } from 'express';
+import userRoutes from './routes/user-routes';  // Assure-toi que le chemin est correct
+import cors from 'cors';
+
+
 
 const app = express();
+
+
+app.use(cors())
+
+// Middleware pour parser le corps de la requête en JSON
 app.use(express.json());
 
-app.use('/users', userRoutes);
+// Définir le préfixe pour les routes utilisateurs
+app.use('/api/users', userRoutes);  // Routes pour les utilisateurs accessibles sous /api/users
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
